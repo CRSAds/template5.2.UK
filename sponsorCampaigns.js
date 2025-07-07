bfe3bb9e45c114955d06d826c934c501a08456d3
CRSAds <yuri@crsadvertising.com>
2025-07-07 13:14:46 +0200

    Update sponsorCampaigns.js

--- sponsorCampaigns.js
+++ sponsorCampaigns.js
@@ -18,6 +18,7 @@ const sponsorCampaigns = {
     sid: 34,
     requiresLongForm: true,
     coregAnswerKey: "coreg_answer_campaign-scottishpower",
+    alwaysSend: false,
     answerFieldKey: "f_2575_coreg_answer_dropdown", // alleen dropdown antwoord
     hasCoregFlow: true
   },
